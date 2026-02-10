3aed35e767978e65233b435ce0bf5c9682a46f09
pornimakolhe111 <pornimakolhe111@gmail.com>
2026-02-10 20:30:04 +0530

    added services

---
+++ Service.java
@@ -0,0 +1,17 @@
+package com.student.service;
+import com.student.bean.Grade;
+import com.student.bean.Student;
+import java.util.ArrayList;
+
+// service interface: includes all service methods for managing student.
+public interface Service {
+    Student createStudent(int roll_number, String name);
+    double calculatePercentage(Student student);
+    Grade calculateGrade(double percentage);
+    void addStudent(int roll_number, String name);
+    void removeStudent(int roll_number);
+    ArrayList<Integer> acceptMarks();
+    int calculateLowestMarks(Student student);
+    int calculateHighestMarks(Student student);
+    void displayAllStudents();
+}
